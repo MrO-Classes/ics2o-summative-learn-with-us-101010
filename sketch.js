88ca0a778b3260f05dc31108601870b75163df36
Mdfs05 <55096826+Mdfs05@users.noreply.github.com>
2019-12-12 11:53:21 -0500

    sketch.js-2
    The sketch.js file.
    -Added Image, buttons, and some button functionality.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,51 @@
+let img;
+
 function setup() {
   createCanvas(400, 400);
+  img = loadImage('art/SplashScreen.jpg')
 }
 
 function draw() {
   background(100, 150, 200);
+  image(img, 75, 25, 250, 150);
+
+  //Buttons\/
+
+  fill(0, 110, 255);
+
+  //'Click to Play' Button
+  rect(25, 200, 125, 50);
+
+  //'Instructions' Button
+  rect(250, 200, 125, 50);
+
+  //'Controls' Button
+  rect(25, 300, 125, 50);
+
+  //'About Us' Button
+  rect(250, 300, 125, 50);
+
+  fill(0, 0, 0);
+  text("Click to Play", 45, 225);
+  text("Instructions", 275, 225);
+  text("Controls", 45, 325);
+  text("About Us", 270, 325);
+
+  //Debug
+  text("X = " + mouseX + ", Y = " + mouseY, mouseX, mouseY);
+
+  //'Click to Play' Button
+  if (mouseIsPressed && mouseX >= 25 && mouseX <= 150 && mouseY >= 200 && mouseY <= 250) {
+    print("Working-Click-to-Play");
+  }
+
+  //'Instructions' Button
+  if (mouseIsPressed && mouseX >= 250 && mouseX <= 375 && mouseY >= 200 && mouseY <= 250) {
+    print("Working-Instructions");
+  }
+
+  //'Controls' Button
+  if (mouseIsPressed && mouseX >= 25 && mouseX <= 150 && mouseY >= 300 && mouseY <= 350) {
+    print("Working-Controls");
+  }
 }
