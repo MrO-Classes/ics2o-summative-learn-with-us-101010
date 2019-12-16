0e7a8eaf3fe856ddf6dc21e079e7c7492c88c479
Mdfs05 <55096826+Mdfs05@users.noreply.github.com>
2019-12-16 11:53:19 -0500

    sketch.js-3
    The third sketch.js file, the new things that have been added are..
    - Button Functionality
    - Scene Changes
    - Mouse Clicked

--- sketch.js
+++ sketch.js
@@ -9,8 +9,6 @@ function draw() {
   background(100, 150, 200);
   image(img, 75, 25, 250, 150);
   
-  var sceneNum = 0;
-  
   //Buttons\/
 
   fill(0, 110, 255);
@@ -28,36 +26,83 @@ function draw() {
   rect(250, 300, 125, 50);
 
   fill(0, 0, 0);
-  text("Click to Play", 45, 225);
-  text("Instructions", 275, 225);
-  text("Controls", 45, 325);
-  text("About Us", 270, 325);
-  
+  text("Click to Play", 50, 225);
+  text("Instructions", 280, 225);
+  text("Controls", 60, 325);
+  text("About Us", 285, 325);
   
   //Debug
   text("X = " + mouseX + ", Y = " + mouseY, mouseX, mouseY);
-  
-  //'Click to Play' Button
-  if (mouseIsPressed && mouseX >= 25 && mouseX <= 150 && mouseY >= 200 && mouseY <= 250) {
-    print("Working-Click-to-Play");
-    sceneNum = 1;
-  }
 
-  //'Instructions' Button
-  if (mouseIsPressed && mouseX >= 250 && mouseX <= 375 && mouseY >= 200 && mouseY <= 250) {
-    print("Working-Instructions");
-    
-  }
-
-  //'Controls' Button
-  if (mouseIsPressed && mouseX >= 25 && mouseX <= 150 && mouseY >= 300 && mouseY <= 350) {
-    print("Working-Controls");
-    
-  }
-  
-  //'About Us' Button
-  if (mouseIsPressed && mouseX >= 250 && mouseX <= 375 && mouseY >= 300 && mouseY <= 350) {
-    print("Working-About-Us");
-  }
+  drawScene();
   
 }
+
+  //Button Function
+  mouseClicked = function() {
+    
+  //Click to Play/Instructions/Controls/About Us
+  if (mouseX >= 25 && mouseX <= 150 && mouseY >= 200 && mouseY <= 250) {
+    print("Working-Click-to-Play");
+    sceneNum = 1;
+    
+  } 
+    else if (mouseX >= 250 && mouseX <= 375 && mouseY >= 200 && mouseY <= 250) {
+    print("Working-Instructions");
+    sceneNum = 2;
+    
+  } 
+    else if (mouseX >= 25 && mouseX <= 150 && mouseY >= 300 && mouseY <= 350) {
+    print("Working-Controls");
+      sceneNum = 3;
+    
+  } 
+    else if (mouseX >= 250 && mouseX <= 375 && mouseY >= 300 && mouseY <= 350) {
+    print("Working-About-Us");
+      sceneNum = 4;
+    
+  }
+    
+  }
+
+  var sceneNum = 0;
+
+  var drawBackButton = function() {
+    textSize(12);
+    rect(325, 25, 55, 30);
+    fill(255, 255, 255);
+    text("BACK", 335, 45);
+  }
+
+  //To draw scenes - Click to Play/Instructions/Controls/About Us
+  var drawScene = function() {
+    if (sceneNum === 1) {
+      background(255, 0, 0);
+      print("Working-Scene-1");
+    }
+      else if (sceneNum === 2) {
+      background(0, 255, 0);
+      print("Working-Scene-2");
+      drawBackButton();
+      fill(0, 0, 0);
+      textSize(60);
+      text("Instructions", 10, 75);
+    }
+      else if (sceneNum === 3) {
+      background(0, 0, 255);
+      print("Working-Scene-3");
+      drawBackButton();
+      fill(0, 0, 0);
+      textSize(60);
+      text("Controls", 10, 75);
+    }
+      else if (sceneNum === 4) {
+      background(255, 255, 255);
+      print("Working-Scene-4");
+      drawBackButton();
+      fill(0, 0, 0);
+      textSize(60);
+      text("About Us", 10, 75);
+    }
+  }
+  
