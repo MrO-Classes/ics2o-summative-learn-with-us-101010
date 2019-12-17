2b606f609955a62145b1e83d2c55e0735b12373d
Mdfs05 <55096826+Mdfs05@users.noreply.github.com>
2019-12-17 10:29:28 -0500

    sketch.js-4
    The fourth sketch.js file, ~30 lines of code added.
    New things:
    -Buttons work as intended now.
    -Instructions page is nearly complete.

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
 }
 
 function draw() {
-  background(100, 150, 200);
+  background(255, 0, 125);
   image(img, 75, 25, 250, 150);
   
   //Buttons\/
@@ -42,25 +42,31 @@ function draw() {
   mouseClicked = function() {
     
   //Click to Play/Instructions/Controls/About Us
-  if (mouseX >= 25 && mouseX <= 150 && mouseY >= 200 && mouseY <= 250) {
+  if (mouseX >= 25 && mouseX <= 150 && mouseY >= 200 && mouseY <= 250 && sceneNum == 0) {
     print("Working-Click-to-Play");
     sceneNum = 1;
     
   } 
-    else if (mouseX >= 250 && mouseX <= 375 && mouseY >= 200 && mouseY <= 250) {
+    else if (mouseX >= 250 && mouseX <= 375 && mouseY >= 200 && mouseY <= 250 && sceneNum == 0) {
     print("Working-Instructions");
     sceneNum = 2;
     
   } 
-    else if (mouseX >= 25 && mouseX <= 150 && mouseY >= 300 && mouseY <= 350) {
+    else if (mouseX >= 25 && mouseX <= 150 && mouseY >= 300 && mouseY <= 350 && sceneNum == 0) {
     print("Working-Controls");
-      sceneNum = 3;
+    sceneNum = 3;
     
   } 
-    else if (mouseX >= 250 && mouseX <= 375 && mouseY >= 300 && mouseY <= 350) {
+    else if (mouseX >= 250 && mouseX <= 375 && mouseY >= 300 && mouseY <= 350 && sceneNum == 0) {
     print("Working-About-Us");
-      sceneNum = 4;
+    sceneNum = 4;
     
+  }
+    else if(mouseX >= 325 && mouseX <= 380 && mouseY >= 22 && mouseY <= 55 && sceneNum > 0) {
+    print("Working-Back");
+    sceneNum = 0;
+    textSize(12);
+      
   }
     
   }
@@ -73,12 +79,13 @@ function draw() {
     fill(255, 255, 255);
     text("BACK", 335, 45);
   }
-
+  
   //To draw scenes - Click to Play/Instructions/Controls/About Us
   var drawScene = function() {
     if (sceneNum === 1) {
       background(255, 0, 0);
       print("Working-Scene-1");
+      drawBackButton();
     }
       else if (sceneNum === 2) {
       background(0, 255, 0);
@@ -87,6 +94,29 @@ function draw() {
       fill(0, 0, 0);
       textSize(60);
       text("Instructions", 10, 75);
+      textSize(25);
+      text("What To Do:", 10, 125);
+      fill(0, 255, 0);
+      rect(10, 135, 380, 35);
+      fill(0, 0, 0);
+      textSize(15);
+      text("You must guide Sparkles through obstacles, solve math", 15, 150);
+      text("problems and more!", 15, 165);
+      textSize(25);
+      text("Objective:", 10, 200);
+      fill(0, 255, 0);
+      rect(10, 210, 380, 35);
+      fill(0, 0, 0);
+      textSize(15);
+      text("The objective of the game is to make through all the", 15, 225);
+      text("obstacles and get Spakles to the place he loves most.", 15, 240);
+      textSize(25);
+      text("Example of Math Problem:", 10, 275);
+      fill(0, 255, 0);
+      rect(10, 285, 380, 35);
+      fill(0, 0, 0);
+      textSize(16);
+      text("' 2, 8, 12, 16, 20... ' What does the pattern go up by?", 15, 305);
     }
       else if (sceneNum === 3) {
       background(0, 0, 255);
